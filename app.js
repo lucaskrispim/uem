fa5656ae1f8ee0197a5504accdbd890bc782e9f7
lucaskrispim <lucaskrispim@hotmail.com>
2020-06-18 21:39:51 -0300

    problema 8

--- app.js
+++ app.js
@@ -1,14 +1,17 @@
 let app = require('./config/server');
 
-
-/* parametrizar a porta de escuta */
-var port_number = process.env.PORT || 3000;
+/*
+var port_number = process.env.PORT || 3001;
 let server = app.listen(port_number,()=>{
     console.log("Servidor On");
 });
    
 let io = require('socket.io').listen(server);
+*/
+let server = require('http').createServer(app)
+io = require('socket.io').listen(server)
 
+server.listen(process.env.PORT || 3000);
 
 app.set('io',io);
   
@@ -23,7 +26,7 @@ io.on('connection',(socket)=>{
         //let connection = app.config.dbConnection();
         //let mapaModel = new app.app.models.MapaDAO(connection);
         //mapaModel.getPoints((error,result)=>{
-        result=[{'placa':hll6462,'latitude':-1.295243,'longitude':-45.754561}] 
+        result=[{'placa':'hll6462','latitude':-1.295243,'longitude':-45.754561}] 
         socket.emit('msgParaCliente',result);
         //});        
         
