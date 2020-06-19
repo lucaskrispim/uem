27570b5aaea57daa06c5cbf3ca2477a29d95a33d
lucaskrispim <lucaskrispim@hotmail.com>
2020-06-19 10:29:05 -0300

    instalação do express-load

--- config/server.js
+++ config/server.js
@@ -1,8 +1,7 @@
 let express = require('express');
 let consign = require('consign');
 let bodyParser = require('body-parser');
-//let io = require('./ioConnection');
-//const { check, validationResult } = require('express-validator');
+let load = require('express-load');
 
 let app = express();
 app.engine('html',require('ejs').renderFile);
@@ -12,14 +11,18 @@ app.set('views','./app/views');
 app.use(express.static('./app/public'));
 app.use(bodyParser.urlencoded({extended:true}));
 
-//app.use({ check, validationResult }());
-
-//io.socketConnect(server);
-
+/*
 consign().include('./config/dbConnection.js')    
             .then('./app/models')   
             .then('./app/controllers') 
             .then('./app/routes')
             .into(app);
-        
+*/
+
+load('./config/dbConnection.js')
+                .then('./app/models')   
+                .then('./app/controllers') 
+                .then('./app/routes')
+                .into(app);
+
 module.exports = app;
